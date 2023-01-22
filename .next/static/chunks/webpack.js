8d62ef060c5a68033ad47eac47966e8e06f3b781
Abul Basar <adulbashar124@gmail.com>
2023-01-22 18:07:58 +0600

    scss icon add with unicode

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bee1bf0badf76379"; }
+/******/ 		__webpack_require__.h = function() { return "11c76fbe68f2be90"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
